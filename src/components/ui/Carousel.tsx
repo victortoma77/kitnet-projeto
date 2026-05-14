e7ac9c07de31f0060d6be334f896b76c870e025b
victortoma77 <victor456toma@gmail.com>
2026-05-13 21:03:33 -0300

    fixes

--- src/components/ui/Carousel.tsx
+++ src/components/ui/Carousel.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { FC } from 'react';
+import { FC } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/navigation';
